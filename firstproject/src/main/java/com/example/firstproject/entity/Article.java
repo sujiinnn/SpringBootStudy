c3e48bffaaa1830e2f0954d6ef961bf2fd528a3d
sujiinnn <sujiinnn@gmail.com>
2023-08-23 01:22:36 +0900

    JPA 로깅 설정 및 Entity의 id 대표값에 붙은 @GeneratedValue 어노테이션 수정

--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -5,19 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity // DB가 해당 객체를 인식 가능!
+@Entity // DB가 해당 객체를 인식 가능 (해당 클래스로 테이블을 만든다!)
 @AllArgsConstructor
 @NoArgsConstructor // 디폴트 생성자 추가
 @ToString
 @Getter
 public class Article {
     @Id // 대표값을 지정! Like a 주민등록번호
-    @GeneratedValue // 1, 2, 3, .... 자동 생성 어노테이션!
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 id를 자동 생성 어노테이션!
     private Long id;
 
     @Column
